e6e05f95482732849cd8a3b498ed4617bd5ecf76
Ravi Varanasi <rvaranasi@ravi-varanasis-macbook.local>
2010-02-02 00:13:13 -0600

    Change code to read files from directory

--- src/main/java/myprojects/lss/LssCommand.java
+++ src/main/java/myprojects/lss/LssCommand.java
@@ -1,13 +1,21 @@
 package myprojects.lss;
 
+import static org.apache.commons.io.FileUtils.listFiles;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 /*
@@ -27,6 +35,7 @@ public class LssCommand {
 		sequenceGrouper = new SequenceGrouper(5);
 		missingSequenceFinder = new  MissingSequenceFinder(5);
 	}
+
 
 	public HashMultimap<String, MatchedResult> execute() {
 		HashMultimap<String, MatchedResult> map = HashMultimap.create();
@@ -87,17 +96,28 @@ public class LssCommand {
 	}
 	
 	public static void main(String[] args) {
-		String[] fileNames = new String[] { "elem.info", "sd_fx29.0101.rgb",
-				"sd_fx29.0102.rgb", "sd_fx29.0103.rgb", "sd_fx29.0104.rgb", "sd_fx29.0105.rgb", "sd_fx29.0106.rgb",
-				"sd_fx29.0107.rgb", "sd_fx29.0108.rgb", "sd_fx29.0109.rgb", "sd_fx29.0110.rgb", "sd_fx29.0111.rgb",
-				"sd_fx29.0112.rgb", "sd_fx29.0113.rgb", "sd_fx29.0114.rgb", "sd_fx29.0115.rgb", "sd_fx29.0116.rgb", 
-				"sd_fx29.0117.rgb", "sd_fx29.0118.rgb", "sd_fx29.0119.rgb", "sd_fx29.0120.rgb", "sd_fx29.0121.rgb", 
-				"sd_fx29.0123.rgb", "sd_fx29.0124.rgb", "sd_fx29.0125.rgb", "sd_fx29.0126.rgb", "sd_fx29.0127.rgb", 
-				"sd_fx29.0128.rgb", "sd_fx29.0129.rgb", "sd_fx29.0130.rgb", "sd_fx29.0131.rgb", "sd_fx29.0132.rgb", 
-				"sd_fx29.0133.rgb", "sd_fx29.0134.rgb", "sd_fx29.0135.rgb", "sd_fx29.0136.rgb", "sd_fx29.0137.rgb", 
-				"sd_fx29.0138.rgb", "sd_fx29.0139.rgb", "sd_fx29.0140.rgb", "sd_fx29.0141.rgb", "sd_fx29.0142.rgb", 
-				"sd_fx29.0143.rgb", "sd_fx29.0144.rgb", "sd_fx29.0145.rgb", "sd_fx29.0146.rgb", 
-				"sd_fx29.0147.rgb", "strange.xml"};
+		File file = null;
+		if(args.length == 0)
+			file = new File(".");
+		else if(args.length == 1) 
+			file = new File(args[0]);
+		else 
+			System.err.println("Usage: java LssCommand <directory>");
+		if(file == null) {
+			System.err.println(Arrays.toString(args) + " not valid directory");
+			System.exit(-1);
+		}
+		if(!file.isDirectory()) {
+			System.err.println(file.getName() + " is not a directory");
+			System.exit(-1);
+		}
+		File[] files = file.listFiles();
+		List<String> fileNameList = Lists.newArrayList();
+		for(File each: files)
+			if(!each.getName().trim().equals(""))
+				fileNameList.add(each.getName());
+			
+		String[] fileNames = Iterables.toArray(fileNameList, String.class);
 		LssCommand lssCommand = new LssCommand(fileNames);
 		HashMultimap<String, MatchedResult> map = lssCommand.execute();
 		System.out.println(lssCommand.toString(map));
